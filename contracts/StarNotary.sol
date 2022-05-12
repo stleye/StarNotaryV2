edd6c499c15834751af91eb72da8540addf77fab
stleye <stleye@ovloop.com>
2022-05-12 14:01:08 -0300

    Bug Fixes in contract

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -32,24 +32,18 @@ contract StarNotary is ERC721 {
         return payable(address(uint160(x)));
     }
 
-    function buyStar(uint256 _tokenId) public  payable {
+    function buyStar(uint256 _tokenId) public payable {
         require(starsForSale[_tokenId] > 0, "The Star should be up for sale");
-
         uint256 starCost = starsForSale[_tokenId];
         address starOwner = ownerOf(_tokenId);
-
         require(msg.value > starCost, "You need to have enough Ether");
-
-        transferFrom(starOwner, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
-
+        _transfer(starOwner, msg.sender, _tokenId);
         address payable starOwnerPayable = _make_payable(starOwner); // We need to make this conversion to be able to use transfer() function to transfer ethers
         starOwnerPayable.transfer(starCost);
-
         if(msg.value > starCost) { //If there is an extra value that was sent, we return it to the sender
             address payable senderAddressPayable = _make_payable(msg.sender);
             senderAddressPayable.transfer(msg.value - starCost);
         }
-
         starsForSale[_tokenId] = 0;
 
     }
